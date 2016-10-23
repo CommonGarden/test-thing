835fade99176ecd0c08e83c916ed61cdebe50f37
Jake Hartnell <jake.hartnell@gmail.com>
2016-10-22 19:01:19 -0700

    Update Grow.js

--- test-thing.js
+++ test-thing.js
@@ -1,14 +1,15 @@
 // THIS SHOULD BE RUN IN A SEPERATE TERMINAL INSTANCE.
-// Pretend it's a Raspberry pi running the script.
+// (Pretend it's a Raspberry pi running the script.)
 
 // Import the latest build of the Grow.js library
-var GrowInstance = require('Grow.js');
+var GrowInstance = require('./dist/Grow.umd.js');
 
-// Create a new grow instance.
 var grow = new GrowInstance({
-    uuid: '',
-    token: '',
-    webcomponent: '',
+    // Add UUID and Token to connect to Grow-IoT
+    uuid: null,
+    token: null,
+
+    webcomponent: 'test-thing',
 
     // Properties can be updated by the API
     properties: {
@@ -18,13 +19,12 @@ var grow = new GrowInstance({
         }
     },
 
-    // Actions are the API of the thing, these are called from the webcomponent
-    actions: {
-        testbutton: {
-            function: function () {
-                console.log('Test successful.');
-            }
-        }
+    start: function () {
+        console.log('test-thing started');
+    },
+
+    testbutton: function () {
+        console.log('Test successful.');
     }
 });
 
